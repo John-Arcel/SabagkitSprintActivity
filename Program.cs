1ed2ef70daa7cb9282b01cc43f973bde88f7586e
LAB23_USER ON WS35 <L23Y16W35@LabHCI.net>
2026-09-24 16:27:58 +0800

    feat(dashboard): implement interactive post feed with polished card UI

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+    .AddInteractiveServerComponents(); // ENABLES INTERACTIVITY
 
 var app = builder.Build();
 
@@ -12,7 +12,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -22,6 +21,6 @@ app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+    .AddInteractiveServerRenderMode(); // ENABLES INTERACTIVITY
 
 app.Run();
